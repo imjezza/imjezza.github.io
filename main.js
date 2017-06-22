c8759582a03f36f3fea0146fa6ccb5c6db396bf0
Jeremy Cvicker <jcvicker@gmail.com>
2017-06-22 11:19:01 -0500

    add survey page

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
-    $('#nav-list li a').mouseenter(function() {
-        $('#nav-list li a').fadeTo('fast', 1);
-    });
-    $('#nav-list.li').mouseleave(function() {
-        $('#nav-list.li').fadeTo('fast', 0.5);
-    });
-});
+// $(document).ready(function() {
+//     $('#nav-list li a').mouseenter(function() {
+//         $('#nav-list li a').fadeTo('fast', 1);
+//     });
+//     $('#nav-list.li').mouseleave(function() {
+//         $('#nav-list.li').fadeTo('fast', 0.25);
+//     });
+// });
 
